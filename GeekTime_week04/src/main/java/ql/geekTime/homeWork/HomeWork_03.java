e1021d058af9558abe94d7b3fc357c1b0127c596
qianl <agr_2020@163.com>
2021-05-28 16:11:40 +0800

    lock_test

--- GeekTime_week04/src/main/java/ql/geekTime/homeWork/HomeWork_03.java
+++ GeekTime_week04/src/main/java/ql/geekTime/homeWork/HomeWork_03.java
@@ -1,9 +1,12 @@
 package ql.geekTime.homeWork;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class HomeWork_03 {
 
     public static void main(String[] args) {
-
+        Lock lock = new ReentrantLock() ;
         long start=System.currentTimeMillis();
 
         // 在这里创建一个线程或线程池，
@@ -11,19 +14,42 @@ public class HomeWork_03 {
         Runnable R = new Runnable() {
             @Override
             public void run() {
+                lock.lock();  //使用lock锁
                 int result = sum(); //这是得到的返回值
                 System.out.println("异步计算结果为："+result);
+                lock.unlock();
 //                System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
             }
         };
-        Thread t =new Thread(R);
+//        new Thread(){ //使用lock锁
+//            @Override
+//            public void run() {
+//                lock.lock();
+//                int result = sum(); //这是得到的返回值
+//                System.out.println("异步计算结果为："+result);
+//                lock.unlock();
+//            }
+//        }.start();
+        Thread t = new Thread(R);
         t.start();
 //        int result = sum(); //这是得到的返回值
 
         // 确保  拿到result 并输出
 //        System.out.println("异步计算结果为："+result);
-
-        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+//        try {
+//            Thread.sleep(200L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        new Thread(){
+            @Override
+            public void run() {
+                lock.lock();
+                System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+                lock.unlock();
+            }
+        }.start();
+//        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
 
         // 然后退出main线程
     }
